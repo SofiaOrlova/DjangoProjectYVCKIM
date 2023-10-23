ca8e631d5c844a38489f207be5efaac51214320c
SofiaOrlova <son020802@mail.ru>
2023-10-23 15:01:06 +0300

    registration

--- djangoproject/users/forms.py
+++ djangoproject/users/forms.py
@@ -11,12 +11,15 @@ class UserCreationForm(UserCreationForm):
         max_length=254,
         widget=forms.EmailInput(attrs={'autocomplete': 'email'})
     )
-
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control', 'placeholder': 'Введите имя пользователя' }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Введите пароль' }))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control' }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control' }))
+    username = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control' }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={ 'class': 'form-control' }))
+    # username = forms.CharField(widget=forms.TextInput(attrs={
+    #     'class': 'form-control' }))
+    # password = forms.CharField(widget=forms.PasswordInput(attrs={
+    #     'class': 'form-control'}))
         
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email")
+        fields = ("first_name", "last_name", "username", "email")
