d4ec3c100f25d1b411b5e630c4d90dcae04ca182
SofiaOrlova <son020802@mail.ru>
2023-10-24 21:09:35 +0300

    email_verify

--- djangoproject/users/forms.py
+++ djangoproject/users/forms.py
@@ -1,11 +1,46 @@
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.forms import (
+    UserCreationForm as DjangoUserCreationForm,
+    AuthenticationForm as DjangoAuthenticationForm,
+)
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+
+from users.utils import send_email_for_verify
+
 
 User = get_user_model()
 
-class UserCreationForm(UserCreationForm):
+class AuthenticationForm(DjangoAuthenticationForm):
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            self.user_cache = authenticate(
+                self.request,
+                username=username,
+                password=password,
+            )
+            
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+            if not self.user_cache.email_verify:
+                send_email_for_verify(self.request, self.user_cache)
+                raise ValidationError(
+                    'Email not verify, check your email',
+                    code='invalid_login',
+                )
+
+        return self.cleaned_data
+
+
+class UserCreationForm(DjangoUserCreationForm):
     email = forms.EmailField(
         label = ("Email"),
         max_length=254,
@@ -20,6 +55,6 @@ class UserCreationForm(UserCreationForm):
     # password = forms.CharField(widget=forms.PasswordInput(attrs={
     #     'class': 'form-control'}))
         
-    class Meta(UserCreationForm.Meta):
+    class Meta(DjangoUserCreationForm.Meta):
         model = User
         fields = ("first_name", "last_name", "username", "email")
--- djangoproject/users/views.py
+++ djangoproject/users/views.py
@@ -1,7 +1,39 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.shortcuts import redirect, render
 from django.views import View
-from users.forms import UserCreationForm
+from django.utils.http import urlsafe_base64_decode
+from django.core.exceptions import ValidationError
+from django.contrib.auth.tokens import default_token_generator as token_generator
+from django.contrib.auth.views import LoginView
+from users.forms import UserCreationForm, AuthenticationForm
+from users.utils import send_email_for_verify
+
+User = get_user_model()
+
+class MyLoginView(LoginView):
+    form_class = AuthenticationForm
+
+class EmailVerify(View):
+    
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+
+        if user is not None and token_generator.check_token(user, token):
+            user.email_verify = True
+            user.save()
+            login(request, user)
+            return redirect('home')
+        return redirect('invalid_verify')
+
+    @staticmethod
+    def get_user(uidb64):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError,
+                User.DoesNotExist, ValidationError):
+            user = None
+        return user
 
 
 class Register(View):
@@ -19,11 +51,11 @@ class Register(View):
 
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
+            user = authenticate(email=email, password=password)
+            send_email_for_verify(request, user)
+            return redirect('confirm_email')
         context = {
             'form': form 
         }
