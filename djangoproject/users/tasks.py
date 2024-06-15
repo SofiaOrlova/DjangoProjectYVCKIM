1dc87bd6a625f3f23a30bb208e5b1a6a8a4546f5
kimoff2 <vanorlov@gmail.com>
2024-06-15 03:33:36 +0300

    prod prepair

--- djangoproject/users/tasks.py
+++
@@ -1,8 +0,0 @@
-# from django_crontab import crontab, periodic_task
-# from .reminder_email import send_reminder_emails
-
-# @periodic_task(run_every=(crontab(hour=0, minute=0))) 
-# def daily_reminder():
-#     send_reminder_emails()
-
-
