ebcbdaa8547595429d1953a2373283d84578d4a2
SofiaOrlova <98403081+SofiaOrlova@users.noreply.github.com>
2024-07-30 13:01:30 +0300

    Update settings.py

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -135,17 +135,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-
-EMAIL_HOST_USER = 'admin@yvc-kim.ru'
-EMAIL_HOST_PASSWORD = 'wznjbkbyvcxeareu'
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SERVER_EMAIL = EMAIL_HOST_USER
-EMAIL_ADMIN = EMAIL_HOST_USER
-
-BACKGROUND_TASK_RUN_ASYNC = True
-
